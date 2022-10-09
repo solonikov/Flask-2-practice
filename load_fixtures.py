90b88bcdf55067ae0fe5b961991bbc6e905ca625
Ivan <ivan.solonikov@gmail.com>
2022-10-09 13:15:33 +0300

    IMPORTANT: a new database has been created, and the previous one has been deleted. You can access the old database through previous commits. Notes fixture file was corrected and load_fixtures.py was updated (according to the file from teacher's repo)

--- load_fixtures.py
+++ load_fixtures.py
@@ -1,60 +1,36 @@
-import sqlite3
-import json
 import click
-
+import json
+from config import Config
 from api import db
 from api.schemas.user import UserRequestSchema
-from config import Config
 
 from api.models.note import NoteModel
 from api.models.user import UserModel
+from api.models.tag import TagModel
 
-# from sqlalchemy.exc import IntegrityError
-
-
-# with open(path_to_fixture, "r", encoding="UTF-8") as f:
-#     users_data = UserRequestSchema(many=True).loads(f.read())
-#     for user_data in users_data:
-#         user = UserModel(**user_data)
-#         db.session.add(user)
-#     db.session.commit()
-#     print(f"{len(users_data)} users created")
 
 @click.command
-# @click.argument('file_name')
-@click.option('--fixture', help='fixture file name')
+# @click.argument('file_name') # python load_fixtures.py 'notes.json'
+@click.option('--fixture', help='fixture file name') # python load_fixtures.py --fixture 'notes.json'
 def load_data(fixture):
+    models = {
+        "UserModel": UserModel,
+        "NoteModel": NoteModel,
+        "TagModel": TagModel
+    }
     with open(Config.PATH_TO_FIXTURES / fixture, "r", encoding="UTF-8") as f:
         file_data = json.load(f)
         model_name = file_data["model"]
-        if model_name == "UserModel":
-            model = UserModel
-        elif model_name == "NoteModel":
-            model = NoteModel
-
+        model = models[model_name]
         for obj_data in file_data["data"]:
             obj = model(**obj_data)
             db.session.add(obj)
         db.session.commit()
+    print(f"{len(file_data['data'])} {model_name} created")
 
-        print(f"{len(file_data['data'])} objects created")
-
-        # users_data = UserRequestSchema(many=True).loads(f.read())
-        # n = 0
-        # for user_data in users_data:
-        #     user = UserModel(**user_data)
-        #     try:
-        #         db.session.add(user)
-        #         db.session.commit()
-        #         n += 1
-        #     except IntegrityError:  # Обработка ошибки "создание пользователя с НЕ уникальным именем"
-        #         db.session.rollback()
-        # print(f"{n} users created")
 
 # path_to_fixture = "fixtures/notes.json"
 # load_data(path_to_fixture)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     load_data()
-
-
